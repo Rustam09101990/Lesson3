82b32ffc7edddc3c5e8600d424bdd745bcadb602
Rustam Azimov <azimovrstm@gmail.com>
2023-12-28 23:32:37 +0300

    add new console

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { -4, -3, -9, 6, -8, 12, 22, -19, 56, 34 };
+int result = 0;
+foreach (int number in numbers)
+{
+    if (number > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine($"число элементов больше нуля: {result}");
